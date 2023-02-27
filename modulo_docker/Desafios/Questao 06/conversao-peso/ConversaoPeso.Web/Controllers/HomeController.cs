a502a93d9f27f8d3cf896fe376848d49c3e917a4
matheusdguerra <matheusguerra@outlook.com>
2023-02-27 15:16:27 -0300

    Questao 06

---
+++ modulo_docker/Desafios/Questao 06/conversao-peso/ConversaoPeso.Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ConversaoPeso.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string selectTemp, decimal valorRef)
+        {
+            if (selectTemp.Equals("1"))
+            {
+                ViewData["unidade"] = "quilo";
+                ViewData["valor"] = valorRef / 1000;
+            }
+            else
+            {
+                ViewData["unidade"] = "grama";
+                ViewData["valor"] = valorRef * 1000;
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+    }
+}
